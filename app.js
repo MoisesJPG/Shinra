a599f7539a073f64f8228266851be47258806164
MoisesJPG <mojupega1610@gmail.com>
2024-01-30 13:26:11 +0100

    Summary

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var database = GetDatabase();
-
+var activePageId = "planner";
 // Cargar el último valor almacenado al cargar la página
 window.onload = function() {
     console.log(database);
     for (const name in database.characters) {
-        const character = database.characters[name];
         addToCharacterContainer(name);
     }
+    OnEndLoad();
 };
+function OnEndLoad(){
+    document.getElementById('loading').style.display = 'none';
+    document.getElementById(activePageId).style.display = '';
+}
 function changeCharacterElement(name, elementName, This){
     const character = database.characters[name];
     var nextElement = "";
@@ -23,9 +27,7 @@ function changeCharacterElement(name, elementName, This){
             isNext = true;
         }
     }
-    if(isNext){
-        nextElement = 0;
-    }
+    if(isNext) { nextElement = 0; }
     document.getElementsByName(`${name}-Quality${character.quality}-Element${elementName}-Id${character.id}`)[0].getElementsByClassName("data")[thisElement].style.display = "none";
     document.getElementsByName(`${name}-Quality${character.quality}-Element${elementName}-Id${character.id}`)[0].getElementsByClassName("data")[nextElement].style.display = "";
     document.getElementsByName(`${name}-Quality${character.quality}-Element${elementName}-Id${character.id}`)[0].setAttribute('name',`${name}-Quality${character.quality}-Element${character.element[nextElement].name}-Id${character.id}`);
@@ -35,20 +37,17 @@ function changeCharacterElement(name, elementName, This){
 function addToCharacterContainer(name){
     const character = database.characters[name];
     var prefab = ``;
+    var count = 0;
     prefab += `<item name="${name}-Quality${character.quality}-Element${character.element[0].name}-Id${character.id}">`;
     prefab += `    <h2 class="name">${name}</h2>`;
+    prefab += `    <button class="move" type="button">+</button>`;
     prefab += `    <div class="images">`;
     prefab += `        <img onclick="changeCharacterElement('${name}','${character.element[0].name}',this)" class="image-element" src="assets/element/${character.element[0].name} Icon.webp" alt="">`;
     prefab += `        <img class="image-icon" src="assets/character/${name} Icon.webp" alt="">`;
     prefab += `        <img class="image-weapon" src="assets/weaponType/${character.weaponType} Icon.webp" alt="">`;
     prefab += `    </div>`;
-    var count = 0;
     for (const elementId in character.element) {
-        if(count == 0){
-            prefab += `    <div class="data">`;
-        }else{
-            prefab += `    <div class="data" style="display:none">`;
-        }
+        if(count == 0){ prefab += `<div class="data">`; } else { prefab += `<div class="data" style="display:none">`; }
         prefab += `        <p class="data-col1">`;
         prefab += `            <strong>Level</strong><br><span class="level">${character.level.from} > ${character.level.to}</span><br>`;
         prefab += `            <strong>Constellation</strong><br><span class="constellation">${character.element[elementId].constellations.from} > ${character.element[elementId].constellations.to}</span>`;
@@ -59,51 +58,4 @@ function addToCharacterContainer(name){
     }
     prefab += `</item>`;
     document.getElementById("characters-container").getElementsByTagName("container")[0].innerHTML += prefab;
-}
-function saveData() { localStorage.setItem('mainData', database); }
-
-// Función para exportar los datos almacenados
-function exportData() {
-    const jsonData = JSON.stringify(localStorage.getItem('mainData'));
-    const enlaceDescarga = document.createElement('a');
-    enlaceDescarga.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(jsonData);
-    enlaceDescarga.download = 'datos_exportados.json';
-    enlaceDescarga.click();
-}
-
-// Función para importar datos desde un archivo JSON
-function importData() {
-    const inputImportar = document.createElement('input');
-    inputImportar.type = 'file';
-
-    inputImportar.addEventListener('change', function() {
-        const archivo = inputImportar.files[0];
-        
-        if (archivo) {
-            const lector = new FileReader();
-
-            lector.onload = function(e) {
-                try {
-                    const datosImportados = JSON.parse(e.target.result);
-
-                    localStorage.setItem('mainData', datosImportados);
-                    console.log(datosImportados);
-                } catch (error) {
-                    alert('Error al leer el archivo.');
-                }
-            };
-
-            lector.readAsText(archivo);
-        }
-    });
-
-    // Simular clic en el input para abrir el explorador de archivos
-    inputImportar.click();
-}
-
-// Función para borrar todos los datos almacenados
-function clearData() {
-    localStorage.clear();
-    alert('Todos los datos han sido borrados.');
-    window.location.reload();
 }
--- database.js
+++ database.js
@@ -1,114 +1,117 @@
 var database = {}
-var lastCharacterId = 6;
+var lastCharacterId = -1;
 function GetDatabase(){
-    database = {
-        "user": {
-            "username": "",
-            "character": "",
-            "birthday": ""
-        },
-        "characters": {
-            "Traveler": {
-                "id": 0,
-                "quality": 5,
-                "weaponType": "Sword",
-                "active": true,
-                "level": { "from": 1, "to": 90 },
-                "frinedship": null,
-                "element": [
-                    {
-                        "name": "Anemo",
-                        "constellations": { "from": 0, "to": 6, "basic": null, "elemental": 0, "burst": 0},
-                        "talents": {
-                            "basic": {"from":1,"to":10},
-                            "elemental": {"from":1,"to":10},
-                            "burst": {"from":1,"to":10}
-                        }
-                    }, {
-                        "name": "Geo",
-                        "constellations": { "from": 0, "to": 6, "basic": null, "elemental": 0, "burst": 0},
-                        "talents":{
-                            "basic": {"from":1,"to":10},
-                            "elemental": {"from":1,"to":10},
-                            "burst": {"from":1,"to":10}
-                        }
-                    }, {
-                        "name": "Electro",
-                        "constellations": { "from": 0, "to": 6, "basic": null, "elemental": 0, "burst": 0},
-                        "talents":{
-                            "basic": {"from":1,"to":10},
-                            "elemental": {"from":1,"to":10},
-                            "burst": {"from":1,"to":10}
-                        }
-                    }, {
-                        "name": "Dendro",
-                        "constellations": { "from": 0, "to": 6, "basic": null, "elemental": 0, "burst": 0},
-                        "talents":{
-                            "basic": {"from":1,"to":10},
-                            "elemental": {"from":1,"to":10},
-                            "burst": {"from":1,"to":10}
-                        }
-                    }, {
-                        "name": "Hydro",
-                        "constellations": { "from": 0, "to": 6, "basic": null, "elemental": 0, "burst": 0},
-                        "talents":{
-                            "basic": {"from":1,"to":10},
-                            "elemental": {"from":1,"to":10},
-                            "burst": {"from":1,"to":10}
-                        }
-                    }, {
-                        "name": "Pyro",
-                        "constellations": { "from": 0, "to": 6, "basic": null, "elemental": 0, "burst": 0},
-                        "talents":{
-                            "basic": {"from":1,"to":10},
-                            "elemental": {"from":1,"to":10},
-                            "burst": {"from":1,"to":10}
-                        }
-                    }, {
-                        "name": "Cryo",
-                        "constellations": { "from": 0, "to": 6, "basic": null, "elemental": 0, "burst": 0},
-                        "talents":{
-                            "basic": {"from":1,"to":10},
-                            "elemental": {"from":1,"to":10},
-                            "burst": {"from":1,"to":10}
-                        }
-                    }
-                ]
-            }
-        }
-    }
-    AddCharacterToDatabase("Amber",     4, [["Pyro",    null,0,0,0,0,0]], "Bow");
-    AddCharacterToDatabase("Kaeya",     4, [["Cryo",    null,0,0,0,0,0]], "Sword");
-    AddCharacterToDatabase("Lisa",      4, [["Electro", null,0,0,0,0,0]], "Catalyst");
-    AddCharacterToDatabase("Barbara",   4, [["Hydro",   null,0,0,0,0,0]], "Catalyst");
-    AddCharacterToDatabase("Razor",     4, [["Electro", null,0,0,0,0,0]], "Claymore");
-    AddCharacterToDatabase("Xiangling", 4, [["Pyro",    null,0,0,0,0,0]], "Polearm");
-    AddCharacterToDatabase("Beidou",    4, [["Electro", null,0,0,0,0,0]], "Claymore");
-    AddCharacterToDatabase("Xingchiu",  4, [["Hydro",   null,0,0,0,0,0]], "Sword");
-    AddCharacterToDatabase("Ninguang",  4, [["Geo",     null,0,0,0,0,0]], "Catalyst");
-    AddCharacterToDatabase("Fischl",    4, [["Electro", null,0,0,0,0,0]], "Bow");
-    AddCharacterToDatabase("Bennett",   4, [["Pyro",    null,0,0,0,0,0]], "Sword");
-    AddCharacterToDatabase("Noelle",    4, [["Geo",     null,0,0,0,0,0]], "Claymore");
-    AddCharacterToDatabase("Chongyun",  4, [["Cryo",    null,0,0,0,0,0]], "Claymore");
-    AddCharacterToDatabase("Sacarosa",  4, [["Anemo",   null,0,0,0,0,0]], "Catalyst");
-    AddCharacterToDatabase("Jean",      5, [["Anemo",   null,0,0,0,0,0]], "Sword");
-    //AddCharacterToDatabase("", , [""], "");
-    return returnDatabase();
     if(localStorage.getItem("mainData")){
         return JSON.parse(localStorage.getItem("mainData"));
     } else {
+        database = {
+            "user": {
+                "username": "",
+                "character": "",
+                "birthday": ""
+            },
+            "characters": {
+            }
+        }
+        AddCharacterToDatabase('Traveler',              5, [
+            ['Anemo',   null,null,null,null,null,null],
+            ['Geo',     null,null,null,null,null,null],
+            ['Electro', null,null,null,null,null,null],
+            ['Dendro',  null,null,null,null,null,null],
+            ['Hydro',   null,null,null,null,null,null],
+            ['Pyro',    null,null,null,null,null,null],
+            ['Cryo',    null,null,null,null,null,null]
+        ], 'Sword');
+        AddCharacterToDatabase('Amber',                 4, [['Pyro',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Kaeya',                 4, [['Cryo',    null,null,null,null,null,null]], 'Sword');
+        AddCharacterToDatabase('Lisa',                  4, [['Electro', null,null,null,null,null,null]], 'Catalyst');
+        AddCharacterToDatabase('Barbara',               4, [['Hydro',   null,null,null,null,null,null]], 'Catalyst');
+        AddCharacterToDatabase('Razor',                 4, [['Electro', null,null,null,null,null,null]], 'Claymore');
+        AddCharacterToDatabase('Xiangling',             4, [['Pyro',    null,null,null,null,null,null]], 'Polearm');
+        AddCharacterToDatabase('Beidou',                4, [['Electro', null,null,null,null,null,null]], 'Claymore');
+        AddCharacterToDatabase('Xingchiu',              4, [['Hydro',   null,null,null,null,null,null]], 'Sword');
+        AddCharacterToDatabase('Ninguang',              4, [['Geo',     null,null,null,null,null,null]], 'Catalyst');
+        AddCharacterToDatabase('Fischl',                4, [['Electro', null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Bennett',               4, [['Pyro',    null,null,null,null,null,null]], 'Sword');
+        AddCharacterToDatabase('Noelle',                4, [['Geo',     null,null,null,null,null,null]], 'Claymore');
+        AddCharacterToDatabase('Chongyun',              4, [['Cryo',    null,null,null,null,null,null]], 'Claymore');
+        AddCharacterToDatabase('Sacarosa',              4, [['Anemo',   null,null,null,null,null,null]], 'Catalyst');
+        AddCharacterToDatabase('Jean',                  5, [['Anemo',   null,null,null,null,null,null]], 'Sword');
+        AddCharacterToDatabase('Diluc',                 5, [['Pyro',    null,null,null,null,null,null]], 'Claymore');
+        AddCharacterToDatabase('Qiqi',                  5, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Mona',                  5, [['Hydro',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Keching',               5, [['Electro', null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Venti',                 5, [['Anemo',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Klee',                  5, [['Pyro',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Diona',                 4, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Tartaglia',             5, [['Hydro',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Xinyan',                4, [['Pyro',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Zhongli',               5, [['Geo',     null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Albedo',                5, [['Geo',     null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Ganyu',                 5, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Xiao',                  5, [['Anemo',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Hu Tao',                5, [['Pyro',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Rosaria',               4, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Yanfei',                4, [['Pyro',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Eula',                  5, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Kaedehara Kazuha',      5, [['Anemo',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Kamisato Ayaka',        5, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Sayu',                  4, [['Anemo',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Yoimiya',               5, [['Pyro',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Aloy',                  5, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Kujou Sara',            4, [['Electro', null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Shogun Raiden',         5, [['Electro', null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Sangonomiya Kokomi',    5, [['Hydro',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Thoma',                 4, [['Pyro',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Gorou',                 4, [['Geo',     null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Arataki Itto',          5, [['Geo',     null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Yun Jin',               4, [['Geo',     null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Shenhe',                5, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Yae Miko',              5, [['Electro', null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Kamisato Ayato',        5, [['Hydro',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Yelan',                 5, [['Hydro',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Kuki Shinobu',          4, [['Electro', null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Shikanoin Heizou',      4, [['Anemo',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Collei',                4, [['Dendro',  null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Tignari',               5, [['Dendro',  null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Dori',                  4, [['Electro', null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Candace',               4, [['Hydro',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Cyno',                  5, [['Electro', null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Nilou',                 5, [['Hydro',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Nahida',                5, [['Dendro',  null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Laila',                 4, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Faruzan',               4, [['Anemo',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Wanderer',              5, [['Anemo',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Yaoyao',                4, [['Dendro',  null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Alhacen',               5, [['Dendro',  null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Dehya',                 5, [['Pyro',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Mika',                  4, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Kaveh',                 4, [['Dendro',  null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Baizhu',                5, [['Dendro',  null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Kirara',                4, [['Dendro',  null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Lynette',               4, [['Anemo',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Lyney',                 5, [['Pyro',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Freminet',              4, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Neuvillette',           5, [['Hydro',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Wriothesley',           5, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Charlotte',             4, [['Cryo',    null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Furina',                5, [['Hydro',   null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Navia',                 5, [['Geo',     null,null,null,null,null,null]], 'Bow');
+        AddCharacterToDatabase('Chevreuse',             4, [['Pyro',    null,null,null,null,null,null]], 'Bow');
+
+        //AddCharacterToDatabase("", , [""], "");
         localStorage.setItem("mainData",JSON.stringify(database));
         return returnDatabase();
     }
 }
-function returnDatabase(){
-    return database;
-}
+function returnDatabase() { return database; }
 function AddCharacterToDatabase(name, quality, elements, weaponType){
     lastCharacterId++;
     const characterData = {
         [name]: {
             "id": lastCharacterId,
+            "name": name,
+            "order": lastCharacterId,
             "quality": quality,
             "weaponType": weaponType,
             "active": false,
@@ -127,4 +130,50 @@ function AddCharacterToDatabase(name, quality, elements, weaponType){
     };
     // Assuming database is an object
     database.characters = { ...database.characters, ...characterData };
+}
+function saveData() { localStorage.setItem('mainData', JSON.stringify(database)); }
+
+// Función para exportar los datos almacenados
+function exportData() {
+    const jsonData = JSON.stringify(localStorage.getItem('mainData'));
+    const enlaceDescarga = document.createElement('a');
+    enlaceDescarga.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(jsonData);
+    enlaceDescarga.download = 'datos_exportados.json';
+    enlaceDescarga.click();
+}
+
+// Función para importar datos desde un archivo JSON
+function importData() {
+    const inputImportar = document.createElement('input');
+    inputImportar.type = 'file';
+
+    inputImportar.addEventListener('change', function() {
+        const archivo = inputImportar.files[0];
+        
+        if (archivo) {
+            const lector = new FileReader();
+
+            lector.onload = function(e) {
+                try {
+                    const datosImportados = JSON.parse(e.target.result);
+
+                    localStorage.setItem('mainData', datosImportados);
+                    console.log(datosImportados);
+                } catch (error) {
+                    alert('Error al leer el archivo.');
+                }
+            };
+
+            lector.readAsText(archivo);
+        }
+    });
+
+    // Simular clic en el input para abrir el explorador de archivos
+    inputImportar.click();
+}
+
+// Función para borrar todos los datos almacenados
+function clearData() { 
+    localStorage.clear();
+    window.location.reload();
 }
