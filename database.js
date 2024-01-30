47ba39b2fcbf45d045f9dfca9b9b192187624036
MoisesJPG <148658757+MoisesJPG@users.noreply.github.com>
2024-01-30 08:01:27 +0100

    Update database.js

--- database.js
+++ database.js
@@ -77,22 +77,23 @@ function GetDatabase(){
             }
         }
     }
-    AddCharacterToDatabase("Amber", 4, ["Pyro"], "Bow");
-    AddCharacterToDatabase("Kaeya", 4, ["Cryo"], "Sword");
-    AddCharacterToDatabase("Lisa", 4, ["Electro"], "Catalyst");
-    AddCharacterToDatabase("Barbara", 4, ["Hydro"], "Catalyst");
-    AddCharacterToDatabase("Razor", 4, ["Electro"], "Claymore");
-    AddCharacterToDatabase("Xiangling", 4, ["Pyro"], "Polearm");
-    AddCharacterToDatabase("Beidou", 4, ["Electro"], "Claymore");
-    AddCharacterToDatabase("Xingchiu", 4, ["Hydro"], "Sword");
-    AddCharacterToDatabase("Ninguang", 4, ["Geo"], "Catalyst");
-    AddCharacterToDatabase("Fischl", 4, ["Electro"], "Bow");
-    AddCharacterToDatabase("Bennett", 4, ["Pyro"], "Sword");
-    AddCharacterToDatabase("Noelle", 4, ["Geo"], "Claymore");
-    AddCharacterToDatabase("Chongyun", 4, ["Cryo"], "Claymore");
-    AddCharacterToDatabase("Sacarosa", 4, ["Anemo"], "Catalyst");
-    AddCharacterToDatabase("Jean", 5, ["Anemo"], "Sword");
+    AddCharacterToDatabase("Amber",     4, [["Pyro",    null,0,0,0,0,0]], "Bow");
+    AddCharacterToDatabase("Kaeya",     4, [["Cryo",    null,0,0,0,0,0]], "Sword");
+    AddCharacterToDatabase("Lisa",      4, [["Electro", null,0,0,0,0,0]], "Catalyst");
+    AddCharacterToDatabase("Barbara",   4, [["Hydro",   null,0,0,0,0,0]], "Catalyst");
+    AddCharacterToDatabase("Razor",     4, [["Electro", null,0,0,0,0,0]], "Claymore");
+    AddCharacterToDatabase("Xiangling", 4, [["Pyro",    null,0,0,0,0,0]], "Polearm");
+    AddCharacterToDatabase("Beidou",    4, [["Electro", null,0,0,0,0,0]], "Claymore");
+    AddCharacterToDatabase("Xingchiu",  4, [["Hydro",   null,0,0,0,0,0]], "Sword");
+    AddCharacterToDatabase("Ninguang",  4, [["Geo",     null,0,0,0,0,0]], "Catalyst");
+    AddCharacterToDatabase("Fischl",    4, [["Electro", null,0,0,0,0,0]], "Bow");
+    AddCharacterToDatabase("Bennett",   4, [["Pyro",    null,0,0,0,0,0]], "Sword");
+    AddCharacterToDatabase("Noelle",    4, [["Geo",     null,0,0,0,0,0]], "Claymore");
+    AddCharacterToDatabase("Chongyun",  4, [["Cryo",    null,0,0,0,0,0]], "Claymore");
+    AddCharacterToDatabase("Sacarosa",  4, [["Anemo",   null,0,0,0,0,0]], "Catalyst");
+    AddCharacterToDatabase("Jean",      5, [["Anemo",   null,0,0,0,0,0]], "Sword");
     //AddCharacterToDatabase("", , [""], "");
+    return returnDatabase();
     if(localStorage.getItem("mainData")){
         return JSON.parse(localStorage.getItem("mainData"));
     } else {
@@ -114,12 +115,12 @@ function AddCharacterToDatabase(name, quality, elements, weaponType){
             "level": { "from": 1, "to": 90 },
             "friendship": { "from": 1, "to": 10 },
             "element": elements.map(element => ({
-                "name": element,
-                "constellations": { "from": 0, "to": 6, "basic": null, "elemental": 5, "burst": 3 },
+                "name": element[0],
+                "constellations": { "from": 0, "to": 6, "basic": element[1], "elemental": element[2], "burst": element[3] },
                 "talents": {
-                    "basic": { "from": 1, "to": 10 },
-                    "elemental": { "from": 1, "to": 10 },
-                    "burst": { "from": 1, "to": 10 }
+                    "basic": { "from": 1, "to": 10, "added": element[4] },
+                    "elemental": { "from": 1, "to": 10, "added": element[5] },
+                    "burst": { "from": 1, "to": 10, "added": element[6] }
                 }
             }))
         }
